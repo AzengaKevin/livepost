ff1280188c3250e11cd2308e402b468e703e90e3
Azenga Kevin <azenga.kevin7@gmail.com>
2023-03-21 11:57:58 +0300

    WIP: Defining relationshipsbetween the models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Comment extends Model
 {
@@ -12,4 +12,14 @@ class Comment extends Model
     protected $casts = [
         'body' => 'array'
     ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
