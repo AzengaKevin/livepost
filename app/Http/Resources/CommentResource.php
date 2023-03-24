e8aae5b00c49c36b3f124ce17f5ec5c5ff3281be
Azenga Kevin <azenga.kevin7@gmail.com>
2023-03-25 02:41:44 +0300

    WIP: Testing comments rosource routes and JSON comment resource

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -17,6 +17,8 @@ class CommentResource extends JsonResource
         return [
             'id'=> $this->id,
             'body'=> $this->body,
+            'created_at'=> $this->created_at,
+            'updated_at'=> $this->updated_at
         ];
     }
 }
